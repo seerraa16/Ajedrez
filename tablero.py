39e4da1a1dcec32d65116bf25f6526f27ad04763
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-12 19:12:00 +0100

    tablero con espacio con el espacio pedido

--- tablero.py
+++ tablero.py
@@ -1,8 +1,13 @@
 def tablero ():
-    piezas = ["torre", "caballo", "alfil", "reina", "rey", "alfil", "caballo", "torre"],
-    ["peon", "peon", "peon", "peon", "peon", "peon", "peon", "peon"],
+    piezas = [["torre",    "caballo",     "alfil",    "reina",    "rey",     "alfil",    "caballo",  "torre"],
+              ["peon",      "peon",         "peon",     "peon",     "peon",     "peon",     "peon",     "peon"],
+                ["",          "",             "",         "",         "",         "",         "",         ""],
+                ["",          "",             "",         "",         "",         "",         "",         ""],
+                ["",          "",             "",         "",         "",         "",         "",         ""],
+                ["",          "",             "",         "",         "",         "",         "",         ""],
+                ["peon",      "peon",         "peon",     "peon",     "peon",     "peon",     "peon",     "peon"],
+                ["torre",    "caballo",     "alfil",    "reina",    "rey",     "alfil",    "caballo",  "torre"]]
     for i in range(8):
-        print("XO" * 4)
-        print("OX" * 4)
+        print(piezas[i])
 tablero()
 
