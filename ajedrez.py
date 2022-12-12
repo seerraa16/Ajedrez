6224fcd56ce775687d67aff91d373edbccbee899
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-12 21:56:59 +0100

    comentarios explicativos

--- ajedrez.py
+++ ajedrez.py
@@ -23,28 +23,28 @@ def movimiento(tablero):
     if respuesta == "si":
         print("ingrese la posicion de la ficha que desea mover")
         print("ingrese la fila")
-        fila = int(input("fila: "))
+        fila = int(input("fila: ")) #fila de la ficha a mover
         print("ingrese la columna")
-        columna = int(input("columna: "))
+        columna = int(input("columna: ")) #columna de la ficha a mover
         print("ingrese la posicion a la que desea mover la ficha")
         print("ingrese la fila")
-        fila2 = int(input("fila: "))
+        fila2 = int(input("fila: ")) #fila donde queremos que vaya la ficha 
         print("ingrese la columna")
-        columna2 = int(input("columna: "))
-        tablero[fila2][columna2] = tablero[fila][columna]
-        tablero[fila][columna] = " "
-        print(imprimir_tablero(tablero))
+        columna2 = int(input("columna: ")) #columna donde queremos que vaya la ficha
+        tablero[fila2][columna2] = tablero[fila][columna] #movimiento de la ficha que hemos elegido
+        tablero[fila][columna] = " " #al mover la ficha la posicion anterior se queda vacia
+        print(imprimir_tablero(tablero)) #mostramos en pantalla los cambios
         
     else:
         print("Esa no es una respuesta valida")
 print(movimiento(tablero))
-while True:
+while True: #bucle para hacer infinitos movimientos
     print("¿quiere hacer un movimiento?")
     respuesta = input("si/no: ")
     if respuesta == "si":
         print("ingrese la posicion de la ficha que desea mover")
         print("ingrese la fila")
-        fila = int(input("fila: "))
+        fila = int(input("fila: ")) 
         print("ingrese la columna")
         columna = int(input("columna: "))
         print("ingrese la posicion a la que desea mover la ficha")
