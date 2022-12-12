150905a58051e455e9be1d132c9656be680f0d94
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-12 19:16:48 +0100

    creamos lista tablero

--- tablero.py
+++ tablero.py
@@ -1,13 +1,4 @@
-def tablero ():
-    piezas = [["torre",    "caballo",     "alfil",    "reina",    "rey",     "alfil",    "caballo",  "torre"],
-              ["peon",      "peon",         "peon",     "peon",     "peon",     "peon",     "peon",     "peon"],
-                ["",          "",             "",         "",         "",         "",         "",         ""],
-                ["",          "",             "",         "",         "",         "",         "",         ""],
-                ["",          "",             "",         "",         "",         "",         "",         ""],
-                ["",          "",             "",         "",         "",         "",         "",         ""],
-                ["peon",      "peon",         "peon",     "peon",     "peon",     "peon",     "peon",     "peon"],
-                ["torre",    "caballo",     "alfil",    "reina",    "rey",     "alfil",    "caballo",  "torre"]]
-    for i in range(8):
-        print(piezas[i])
-tablero()
+tablero = []
+
+
 
