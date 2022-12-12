420e859d2f835629b288ddec78560fbdfc454022
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-12 19:01:26 +0100

    tBLERO CON FICHAS

---
+++ tablero.py
@@ -0,0 +1,8 @@
+def tablero ():
+    piezas = ["torre", "caballo", "alfil", "reina", "rey", "alfil", "caballo", "torre"],
+    ["peon", "peon", "peon", "peon", "peon", "peon", "peon", "peon"],
+    for i in range(8):
+        print("XO" * 4)
+        print("OX" * 4)
+tablero()
+
